65477755c6a09e18dc3ceddb4b5d0b41a06c29a4
sudeepkrishnan87@gmail.com <sudeepkrishnan87@gmail.com>
2025-06-23 03:19:49 +0530

    initialize Spring Boot project with Gradle setup and basic configuration

---
+++ creational/src/main/java/org/mytechexp/creational/creational/CreationalApplication.java
@@ -0,0 +1,13 @@
+package org.mytechexp.creational.creational;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreationalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CreationalApplication.class, args);
+    }
+
+}
---
+++ creational/src/test/java/org/mytechexp/creational/creational/CreationalApplicationTests.java
@@ -0,0 +1,13 @@
+package org.mytechexp.creational.creational;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreationalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
