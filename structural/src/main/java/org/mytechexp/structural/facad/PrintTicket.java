9eb91fdad8d1920ce6f7b9658f877296895671df
sudeepkrishnan87@gmail.com <sudeepkrishnan87@gmail.com>
2025-07-25 01:23:38 +0530

    add flyweight pattern implementation for tree management in a forest

---
+++ structural/src/main/java/org/mytechexp/structural/facad/PrintTicket.java
@@ -0,0 +1,8 @@
+package org.mytechexp.structural.facad;
+
+public class PrintTicket {
+    public String printTicket(String movieName, int rowNumber, int seatNumber) {
+        // Simulate ticket printing
+        return "Ticket for " + movieName + " has been printed. Row: " + rowNumber + ", Seat: " + seatNumber;
+    }
+}
